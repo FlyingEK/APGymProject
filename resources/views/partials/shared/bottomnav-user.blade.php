12849bf68acffe59325ef6a4986b032b6059723c
Neong Yee Kay <yeekay2002@hotmail.com>
2024-06-27 01:06:56 +0800

    ahref nav

--- resources/views/partials/shared/bottomnav-user.blade.php
+++ resources/views/partials/shared/bottomnav-user.blade.php
@@ -4,10 +4,10 @@
         <a href="{{route('equipment-index')}}"><span class="whiteIcon material-symbols-outlined">home</span><br>Home</a>        
     </div>
     <div class = " navtab col col-sm">
-        <a href="{{route('analytic-report')}}"><span class="whiteIcon material-symbols-outlined">exercise</span><br>Workout</a>        
+        <a href="#"><span class="whiteIcon material-symbols-outlined">exercise</span><br>Workout</a>        
     </div>
     <div class = "navtab col col-sm">
-        <a href="#"><span class="whiteIcon material-symbols-outlined">analytics</span><br>Analytics</a>        
+        <a href="{{route('analytic-report')}}"><span class="whiteIcon material-symbols-outlined">analytics</span><br>Analytics</a>        
     </div>
     <div class = "navtab col col-sm">
         <a href="#"><span class="whiteIcon material-symbols-outlined">person</span><br>Profile</a>        
